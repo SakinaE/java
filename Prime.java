d21006b0b2c3fdfd054f8697b96e8eb07791ff08
SAKINA E <sakinadass850@gmail.com>
2026-01-27 21:39:04 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n, i = 2;
+
+        System.out.print("Enter number: ");
+        n = sc.nextInt();
+
+        while (i < n) {
+            if (n % i == 0) {
+                System.out.println("Not Prime");
+                return;
+            }
+            i++;
+        }
+
+        System.out.println("Prime");
+    }
+}
+
