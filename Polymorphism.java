3ed6a3e6150ad07511bcb5601b4b692b4ae85aa2
SAKINA E <sakinadass850@gmail.com>
2026-02-05 10:20:06 +0530

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,19 @@
+public class Polymorphism {
+
+    public static void main(String[] args) {
+        System.out.println(method1());
+        System.out.println(method2());
+
+    }
+
+    public static int method1() {
+        int z = 5;
+        return z;
+    }
+        public static int method2() {
+            int s = 5;
+            return s;
+
+    }
+
+}
