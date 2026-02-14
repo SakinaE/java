2b86c136e9e10b31110d36aec09cf81f6c584e0d
SAKINA E <sakinadass850@gmail.com>
2026-02-14 19:14:11 +0530

    Add files via upload

---
+++ charstr.java
@@ -0,0 +1,14 @@
+public class charstr {
+
+    public static void main(String[] args) {
+
+
+        char ch = 'x';
+
+
+        String str = Character.toString(ch);
+
+        System.out.println(str);
+
+
+    }}
