385ef8ee2b13380dad320bda36a4bf4bbdb20b19
SAKINA E <sakinadass850@gmail.com>
2026-03-06 20:20:34 +0530

    Add files via upload

---
+++ smallaz.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public   class smallaz {
+
+    public static void main(String[] args) {
+
+        for (int i = 97; i<= 122; i++) {
+            System.out.print((char)i +" ");
+        }
+    }
+}
